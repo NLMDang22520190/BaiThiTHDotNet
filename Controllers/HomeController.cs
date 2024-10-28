e138647acac9d1bf274e89060aa3ce5068881dcd
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-10-28 13:36:07 +0700

    add productsimages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,14 @@
 using System.Diagnostics;
 using BaiThiTHDotNet.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
 
 namespace BaiThiTHDotNet.Controllers
 {
     public class HomeController : Controller
     {
+        QlbanVaLiContext db = new QlbanVaLiContext();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -13,9 +16,15 @@ namespace BaiThiTHDotNet.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? page)
         {
-            return View();
+            int pageSize = 8;
+            int pageNuimber = page == null || page < 0 ? 1 : page.Value;
+
+            var listsanpham = db.TDanhMucSps.AsNoTracking().OrderBy(x => x.TenSp);
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(listsanpham, pageNuimber, pageSize);
+
+            return View(lst);
         }
 
         public IActionResult Privacy()
